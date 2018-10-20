443776f7aeb94e1d782d196a20707424b7ad0e0e
W Song <wmsong@ws-macbook-pro>
2018-10-20 09:24:57 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by W Song on 10/20/18.
+//  Copyright © 2018 W Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
