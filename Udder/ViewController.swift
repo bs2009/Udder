886111d19eba49686b36e3143a98e297000dd147
W Song <wmsong@ws-macbook-pro>
2018-10-20 10:50:56 -0500

    add button function

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollBtnTapped(_ sender: UIButton) {
+        diceImage.backgroundColor = UIColor.blue
+    }
+    
 }
 
